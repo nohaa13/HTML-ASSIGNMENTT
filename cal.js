68ab57d03b765be4cf0b231dc9c9b2fc60ec253a
nohaa13 <154223986+nohaa13@users.noreply.github.com>
2024-02-05 14:57:23 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,13 @@
+function display(val){
+    document.getElementByld('result').value +=val
+    return val
+}
+function solve(){
+    let x=document.getElementByld('result').value
+    let y=eval(x);
+    document.getElementByld('result').value=y
+    return y
+}
+function clearScreen(){
+    document.getElementByld('result').value="
+}
